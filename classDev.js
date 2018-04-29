60e91b54c7ee03b7d8d1348645eb6802039e95b9
emacslisp <java.gdb@gmail.com>
2018-04-29 19:55:11 +1000

    upgrade tsc and get set

--- classDev.js
+++ classDev.js
@@ -1,3 +1,13 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Person = /** @class */ (function () {
     function Person(name, username) {
         this.username = username;
@@ -18,3 +28,41 @@ console.log(person);
 console.log(person.name, person.username);
 person.printAge();
 person.setType("cool guy");
+var Max = /** @class */ (function (_super) {
+    __extends(Max, _super);
+    function Max(username) {
+        var _this = _super.call(this, "Max", username) || this;
+        _this.name = "Max";
+        _this.age = 31;
+        return _this;
+    }
+    return Max;
+}(Person));
+var max = new Max("Anna");
+console.log(max);
+// Getters & Setters
+var Plant = /** @class */ (function () {
+    function Plant() {
+        this._species = "Default";
+    }
+    Object.defineProperty(Plant.prototype, "species", {
+        get: function () {
+            return this._species;
+        },
+        set: function (value) {
+            if (value.length > 3) {
+                this._species = value;
+            }
+            else {
+                this._species = "Default";
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Plant;
+}());
+var plant = new Plant();
+console.log(plant.species);
+plant.species = "test";
+console.log(plant.species);
--- classDev.ts
+++ classDev.ts
@@ -23,3 +23,38 @@ console.log(person.name, person.username);
 
 person.printAge();
 person.setType("cool guy");
+
+class Max extends Person {
+    name = "Max";
+    constructor(username: string) {
+        super("Max", username);
+        this.age = 31;
+    }
+}
+
+const max = new Max("Anna");
+console.log(max);
+
+
+// Getters & Setters
+
+class Plant {
+    private _species: string = "Default";
+
+    get species() {
+        return this._species;
+    }
+
+    set species(value: string) {
+        if(value.length > 3) {
+            this._species = value;
+        } else {
+            this._species = "Default";
+        }
+    }
+}
+
+let plant = new Plant();
+console.log(plant.species);
+plant.species = "test";
+console.log(plant.species);
