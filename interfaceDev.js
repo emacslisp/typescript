79496026192190c4298fd8ab2bd28e9ea23ecdb9
emacslisp <java.gdb@gmail.com>
2018-04-29 23:19:32 +1000

    interface class and property

---
+++ interfaceDev.js
@@ -0,0 +1,29 @@
+function greet(person) {
+    console.log("Hello, " + person.firstName);
+}
+function changeName(person) {
+    person.firstName = "Anna";
+}
+var person = {
+    firstName: "Max",
+    hobbies: ["Cooking", "Sports"],
+    greet: function (lastName) {
+        console.log("Hi, I am " + this.firstName + " " + lastName);
+    }
+};
+//greet({firstName: "Max", age: 27});
+changeName(person);
+greet(person);
+person.greet("Anything");
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    Person.prototype.greet = function (lastName) {
+        console.log("Hi, I am " + this.firstName + " " + lastName);
+    };
+    return Person;
+}());
+var myPerson = new Person();
+myPerson.firstName = "Maximilian";
+greet(myPerson);
+myPerson.greet("Anything");
---
+++ interfaceDev.ts
@@ -0,0 +1,42 @@
+interface NamedPerson {
+    firstName: string;
+    age?: number;
+    [propName: string]: any;
+    greet(lastName: string): void;
+}
+
+function greet(person: NamedPerson) {
+    console.log("Hello, " + person.firstName);
+}
+
+function changeName(person: NamedPerson) {
+    person.firstName = "Anna";
+}
+
+const person: NamedPerson = {
+    firstName: "Max",
+    hobbies: ["Cooking", "Sports"],
+    greet(lastName: string) {
+        console.log("Hi, I am " + this.firstName + " " + lastName);
+    }
+};
+
+
+//greet({firstName: "Max", age: 27});
+changeName(person);
+greet(person);
+person.greet("Anything");
+
+class Person implements NamedPerson {
+    firstName: string;
+    greet(lastName: string) {
+        console.log("Hi, I am " + this.firstName + " " + lastName);            
+    }
+}
+
+
+const myPerson = new Person();
+myPerson.firstName = "Maximilian";
+greet(myPerson);
+
+myPerson.greet("Anything");
